21497bcbd4d6b96230229c7f9225cac66226f6d5
Chuba Ezeigbo <chuba.ezeigbo@gmail.com>
2024-03-26 21:42:46 +0000

    Rebuilt sql script to create database

--- SEOProject/Sql/database_script.sql
+++ SEOProject/Sql/database_script.sql
@@ -1,15 +1,15 @@
 USE [master]
 GO
-/****** Object:  Database [db_searchEngine]    Script Date: 25/03/2024 17:09:41 ******/
+/****** Object:  Database [db_searchEngine]    Script Date: 26/03/2024 21:33:47 ******/
 CREATE DATABASE [db_searchEngine]
  CONTAINMENT = NONE
  ON  PRIMARY 
-( NAME = N'db_searchEngine', FILENAME = N'I:\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\db_searchEngine.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+( NAME = N'db_searchEngine', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\db_searchEngine.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
  LOG ON 
-( NAME = N'db_searchEngine_log', FILENAME = N'I:\Microsoft SQL Server\MSSQL15.MSSQLSERVER2019\MSSQL\DATA\db_searchEngine_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT
+( NAME = N'db_searchEngine_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\db_searchEngine_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
 GO
-ALTER DATABASE [db_searchEngine] SET COMPATIBILITY_LEVEL = 150
+ALTER DATABASE [db_searchEngine] SET COMPATIBILITY_LEVEL = 160
 GO
 IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
 begin
@@ -60,7 +60,7 @@ ALTER DATABASE [db_searchEngine] SET READ_COMMITTED_SNAPSHOT OFF
 GO
 ALTER DATABASE [db_searchEngine] SET HONOR_BROKER_PRIORITY OFF 
 GO
-ALTER DATABASE [db_searchEngine] SET RECOVERY FULL 
+ALTER DATABASE [db_searchEngine] SET RECOVERY SIMPLE 
 GO
 ALTER DATABASE [db_searchEngine] SET  MULTI_USER 
 GO
@@ -76,13 +76,13 @@ ALTER DATABASE [db_searchEngine] SET DELAYED_DURABILITY = DISABLED
 GO
 ALTER DATABASE [db_searchEngine] SET ACCELERATED_DATABASE_RECOVERY = OFF  
 GO
-EXEC sys.sp_db_vardecimal_storage_format N'db_searchEngine', N'ON'
+ALTER DATABASE [db_searchEngine] SET QUERY_STORE = ON
 GO
-ALTER DATABASE [db_searchEngine] SET QUERY_STORE = OFF
+ALTER DATABASE [db_searchEngine] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
 GO
 USE [db_searchEngine]
 GO
-/****** Object:  Table [dbo].[Histories]    Script Date: 25/03/2024 17:09:41 ******/
+/****** Object:  Table [dbo].[Histories]    Script Date: 26/03/2024 21:33:47 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -91,7 +91,7 @@ CREATE TABLE [dbo].[Histories](
 	[HistoryId] [int] IDENTITY(1,1) NOT NULL,
 	[Keyword] [nvarchar](500) NOT NULL,
 	[Url] [nvarchar](1000) NOT NULL,
-	[Position] [int] NOT NULL,
+	[Position] [nvarchar](500) NOT NULL,
 	[SearchTime] [datetime] NOT NULL,
 	[SearchEngine] [nvarchar](20) NOT NULL,
  CONSTRAINT [PK_Histories] PRIMARY KEY CLUSTERED 
